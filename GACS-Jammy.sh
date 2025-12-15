e839c52aa2a6c8a686201a70d45b2d519a32ccef
schqvho <alvision07@gmail.com>
2025-12-15 20:27:05 +0900

    Update GACS-Jammy.sh

--- GACS-Jammy.sh
+++ GACS-Jammy.sh
@@ -62,10 +62,10 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Check Ubuntu version
-# if [ "$(lsb_release -cs)" != "jammy" ]; then
-#    echo -e "${RED}This script only supports Ubuntu 22.04 (Jammy)${NC}"
-#    exit 1
-# fi
+if [ "$(lsb_release -cs)" != "jammy" ]; then
+    echo -e "${RED}This script only supports Ubuntu 22.04 (Jammy)${NC}"
+    exit 1
+fi
 
 # Print banner
 print_banner
@@ -171,6 +171,7 @@ for service in mongod genieacs-cwmp genieacs-nbi genieacs-fs genieacs-ui; do
 done
 
 echo -e "\n${GREEN}${BOLD}Script execution completed successfully!${NC}"
+
 
 
 
